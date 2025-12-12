48a54875b0439e834d217b23c6a7b58e260d923f
PabloXantini <santinypabloe130@gmail.com>
2025-12-12 02:19:38 -0600

    feat: added registers on users

--- server/services/add_user.php
+++ server/services/add_user.php
@@ -1,4 +1,68 @@
 <?php
     require "../db.php";
-    
+    $first_name = $_POST["FIRSTNAME"];
+    $last_name = $_POST["LASTNAME"];
+    $new_email = $_POST["EMAIL"];
+    $new_password = $_POST["PASSWORD"];
+    $next = "../../public/login.php";
+    $back = "../../public/register.php";
+    if(
+        empty($first_name) ||
+        empty($last_name) ||
+        empty($new_email) ||
+        empty($new_password)
+    ){
+        echo
+        '<script>
+            alert("Por favor, complete todos los campos obligatorios");
+            location.href="'.$back.'";
+        </script>';
+        exit();
+    }
+    if(
+        strlen($first_name) > 64 || 
+        strlen($last_name) > 64 || 
+        strlen($new_email) > 64 || 
+        strlen($new_password) > 64
+    ){
+    echo 
+        '<script>
+            alert("Uno o más campos exceden la longitud máxima permitida");
+            location.href="'.$back.'";
+        </script>';
+        exit();
+    }
+    $check_user = "SELECT * FROM clientes WHERE email = '$username'";
+    $check_query = mysqli_query($CONNECTION, $check_user);
+
+    if(mysqli_num_rows($check_query) > 0){
+        echo 
+        '<script>
+            alert("Un usuario ya está registrado con ese correo, por favor elija otro e intente de nuevo");
+            location.href="'.$back.'";
+            </script>';
+        exit();
+    }
+
+    $add_user = "INSERT INTO clientes (id_rol, nombres, apellidos, email, contrasenia) VALUES (
+        '2',
+        '$first_name',
+        '$last_name',
+        '$new_email',
+        '$new_password'
+    )";
+    $query = mysqli_query($CONNECTION, $add_user);
+    if ($query) {
+        echo 
+        '<script>
+            alert("Registro exitoso");
+            location.href="'.$next.'";
+        </script>';
+    } else {
+        echo 
+        '<script>
+            alert("Error al registrarse, por favor intente de nuevo");
+            location.href="'.$back.'";
+        </script>';
+    }
 ?>
